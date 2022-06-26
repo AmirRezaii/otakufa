494451df37cd74d4ec1dbbc07ffd3b76e8d53530
Amir Mohammad Rezaii <amirmohammadrezaii@pop-os.localdomain>
2022-06-27 00:00:30 +0430

    general improvement on everything

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,10 +2,12 @@
 function set_open() {
     $(".menu").addClass("menu-open")
     $(".menu-btn").hide()
+    $("body").css("overflow", "hidden")
 }
 function set_closed() {
     $(".menu").removeClass("menu-open")
     $(".menu-btn").show()
+    $("body").css("overflow", "auto")
 }
 
 // pagination logic for slider
@@ -26,10 +28,17 @@ $(".left-btn").click(() => {
     $(".slider").scrollLeft($(".slider").scrollLeft() + SWIDTH)
 })
 $(".right-btn").click(() => {
-    $(".slider").scrollLeft($(".slider").scrollLeft()-SWIDTH)
+    $(".slider").scrollLeft($(".slider").scrollLeft() - SWIDTH)
 })
 
 function page_btn(e) {
     $(".slider").scrollLeft(SWIDTH*e)
 }
 
+setInterval(() => {
+    if($(".slider").scrollLeft() <= SWIDTH*4) {
+        $(".slider").scrollLeft(0)
+    } else {
+        $(".slider").scrollLeft($(".slider").scrollLeft() + SWIDTH)
+    }
+}, 5000)

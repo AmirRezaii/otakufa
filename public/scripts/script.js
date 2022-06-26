f522f00b3aa31b1e6d5fc19e4f1ecdb76c4c51c6
Amir Mohammad Rezaii <amirmohammadrezaii@pop-os.localdomain>
2022-06-26 15:49:42 +0430

    complete slider

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -7,3 +7,29 @@ function set_closed() {
     $(".menu").removeClass("menu-open")
     $(".menu-btn").show()
 }
+
+// pagination logic for slider
+let SWIDTH = ($(window).width() > 650)? -$(window).width()*60/100 + 40 : -$(window).width() + 40 
+$(window).resize(() => {
+    SWIDTH = ($(window).width() > 650)? -$(window).width()*60/100 + 40 : -$(window).width() + 40
+})
+
+$(".slider").scroll(() => {
+    let e = Math.round($(".slider").scrollLeft() / SWIDTH)
+
+    $(".page-indicator-active").removeClass("page-indicator-active")
+    $("." + e).addClass("page-indicator-active")
+})
+
+// slider left and right buttons
+$(".left-btn").click(() => {
+    $(".slider").scrollLeft($(".slider").scrollLeft() + SWIDTH)
+})
+$(".right-btn").click(() => {
+    $(".slider").scrollLeft($(".slider").scrollLeft()-SWIDTH)
+})
+
+function page_btn(e) {
+    $(".slider").scrollLeft(SWIDTH*e)
+}
+
